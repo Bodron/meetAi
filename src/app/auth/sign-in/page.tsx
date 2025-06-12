3499446ef486576fbe69dfd56ef633ee4262bf13
BodronCosmin <bodron_cosmin2007@yahoo.com>
2025-06-12 13:13:17 +0300

    auth-ui

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    console.log("SignIn Page")
+    return (
+        <SignInView />
+    )
+}
+
+export default Page;
