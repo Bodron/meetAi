11c82861295cc6f1c4a60c84327ee0fb67a01959
BodronCosmin <bodron_cosmin2007@yahoo.com>
2025-06-17 13:51:36 +0300

    auth-socials

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,10 +1,17 @@
-import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+import { auth } from '@/lib/auth'
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-const Page = () => {
-    console.log("SignUp Page")
-    return (
-     <SignUpView />
-    )
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!!session) {
+    redirect('/auth/sign-up')
+  }
+  return <SignUpView />
 }
 
-export default Page;
+export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,21 @@
-"use client"
+import { auth } from '@/lib/auth'
+import { HomeView } from '@/modules/home/ui/views/home-view'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
 
-export default function Home() {
-
-  const { data: session,} = authClient.useSession()
-  const [email, setEmail] = useState("")
-  const [name, setName] = useState("")
-  const [password, setPassword] = useState("")
-  const handleSubmit = () => {
-    authClient.signUp.email({
-      email,
-      password,
-      name,
-    },{
-      onError: (error) => {
-        console.log(error)
-      },
-      onSuccess: () => {
-        console.log("User created")
-      }
-    })
+  if (!session) {
+    redirect('/auth/sign-in')
   }
-  const handleLogin = () => {
-    authClient.signIn.email({
-      email,
-      password,
-      
-    },{
-      onError: (error) => {
-        console.log(error)
-      },
-      onSuccess: () => {
-        console.log("User created")
-      }
-    })
-  }
-
-  if(session){
-    return <div>
-      <h1>Welcome {session.user?.name}</h1>
-      <Button onClick={() => authClient.signOut()}>
-        Sign Out
-      </Button>
-    </div>
-  }
-
   return (
-    <div className="flex flex-col gap-y-10">
-
-    <div className="flex flex-col gap-4">
-      <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-      <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={handleSubmit}>
-        Create User
-      </Button>
-    </div>
-    <div className="flex flex-col gap-4">
-   
-      <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={handleLogin}>
-        Login
-      </Button>
-    </div>
-    </div>
-  );
+    <>
+      <HomeView />
+    </>
+  )
 }
+
+export default Page
